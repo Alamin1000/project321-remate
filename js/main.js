f7f1416880dcedb38bcafc701065152899b85e27
alamin <alaminhosen44000@gmail.com>
2023-05-09 02:08:35 +0600

    fix-small

--- js/main.js
+++ js/main.js
@@ -34,3 +34,44 @@ $(document).ready(function () {
   // preloader
   $("#preloader").fadeOut(500);
 });
+
+
+// slider Scrolling
+sliderScrolling(document.querySelectorAll(".section-slider"), 1, 1);
+function sliderScrolling(sliderSection, showItem, space) {
+  sliderSection.forEach((section) => {
+    let wrapper = section.querySelector(".slider-wrapper");
+    let slides = wrapper.querySelectorAll(".slider-item");
+    let track = wrapper.querySelector(".slider-track");
+    vertical();
+    function vertical() {  
+      wrapper.style.height = window.innerHeight + "px";
+      
+      let itemHeight = wrapper.clientHeight / showItem;
+      let fullheight = itemHeight * slides.length;
+      track.style.height = fullheight + "px";
+
+      slides.forEach((slide) => {
+        slide.style.height = itemHeight + "px";
+      });
+
+      gsap.to(track, {
+        y: -fullheight + itemHeight * showItem,
+        ease: "none",
+        scrollTrigger: {
+          trigger: section,
+          start: "top top",
+          end: fullheight * space,
+          pin: section,
+          scrub: 0.5,
+          snap: {
+            snapTo: 1 / (slides.length - showItem),
+            duration: 0.5,
+            delay: 0,
+            ease: "none",
+          },
+        },
+      });
+    }
+  });
+}
